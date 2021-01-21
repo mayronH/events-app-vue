fcc67c4ce80cde26bd745a52790f706fc263907f
Mayron Sousa <mayron.hen@gmail.com>
2021-01-21 16:11:49 -0300

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <div class="hero is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-size-1">
+                        About Animal Rescue League
+                    </h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <p class="org-description is-size-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p class="org-description is-size-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.org-description {
+    margin-top: 50px;
+}
+</style>
