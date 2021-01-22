da9076f490df225100a39a3eeb0488811b43ae25
Mayron Sousa <mayron.hen@gmail.com>
2021-01-22 16:50:15 -0300

    utilizando API

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -16,6 +16,7 @@
 
 <script>
 import EventCard from "@/components/EventCard";
+import EventService from '@/services/EventService.js';
 
 export default {
     name: "EventsList",
@@ -26,38 +27,19 @@ export default {
     data() {
         return {
             event: {},
-            events: [
-                {
-                    id: 1,
-                    name: "Charity Ball",
-                    category: "Fundraising",
-                    description:
-                        "Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.",
-                    featuredImage: "https://placekitten.com/500/500",
-                    images: [
-                        "https://placekitten.com/500/500",
-                        "https://placekitten.com/500/500",
-                        "https://placekitten.com/500/500",
-                    ],
-                    location: "1234 Fancy Ave",
-                    date: "12-25-2019",
-                    time: "11:30",
-                },
-                {
-                    id: 2,
-                    name: "Rescue Center Goods Drive",
-                    category: "Adoptions",
-                    description:
-                        "Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.",
-                    featuredImage: "https://placekitten.com/500/500",
-                    images: ["https://placekitten.com/500/500"],
-                    location: "1234 Dog Alley",
-                    date: "11-21-2019",
-                    time: "12:00",
-                },
-            ],
+            events: []
         };
     },
+    created(){
+        this.getEventData();
+    },
+    methods: {
+        async getEventData(){
+            EventService.getEvents().then((events) => {
+                this.$set(this, "events", events);
+            }).bind(this)
+        }
+    }
 };
 </script>
 
--- src/views/EventSingle.vue
+++ src/views/EventSingle.vue
@@ -19,14 +19,22 @@
         <section class="event-content">
             <div class="container">
                 <p class="is-size-4 description">{{ event.description }}</p>
-                <p class="is-size-4"><strong>Location:</strong> {{ event.location }}</p>
-                <p class="is-size-4"><strong>Category:</strong> {{ event.category }}</p>
+                <p class="is-size-4">
+                    <strong>Location:</strong> {{ event.location }}
+                </p>
+                <p class="is-size-4">
+                    <strong>Category:</strong> {{ event.category }}
+                </p>
 
                 <div
                     class="event-images columns is-multiline has-text-centered"
                 >
-                    <div v-for="image in event.images" :key="image.id" class="column is-one-third">
-                        <img :src="image" :alt="event.name">
+                    <div
+                        v-for="image in event.images"
+                        :key="image.id"
+                        class="column is-one-third"
+                    >
+                        <img :src="image" :alt="event.name" />
                     </div>
                 </div>
             </div>
@@ -35,48 +43,26 @@
 </template>
 
 <script>
+import EventService from "@/services/EventService.js";
+
 export default {
     name: "EventSingle",
     data() {
         return {
-            events: [
-                {
-                    id: 1,
-                    name: "Charity Ball",
-                    category: "Fundraising",
-                    description:
-                        "Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.",
-                    featuredImage: "https://placekitten.com/500/500",
-                    images: [
-                        "https://placekitten.com/500/500",
-                        "https://placekitten.com/500/500",
-                        "https://placekitten.com/500/500",
-                    ],
-                    location: "1234 Fancy Ave",
-                    date: "12-25-2019",
-                    time: "11:30",
-                },
-                {
-                    id: 2,
-                    name: "Rescue Center Goods Drive",
-                    category: "Adoptions",
-                    description:
-                        "Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.",
-                    featuredImage: "https://placekitten.com/500/500",
-                    images: ["https://placekitten.com/500/500"],
-                    location: "1234 Dog Alley",
-                    date: "11-21-2019",
-                    time: "12:00",
-                },
-            ],
             event: {},
         };
     },
 
     created() {
-        const ID = Number(this.$route.params.id);
-        let event = this.events.find((event) => event.id === ID);
-        this.event = event;
+        this.getEventData();
+    },
+
+    methods: {
+        async getEventData() {
+            EventService.getEventSingle(this.$route.params.id).then((event) => {
+                this.$set(this, "event", event);
+            }).bind(this);
+        },
     },
 };
 </script>
